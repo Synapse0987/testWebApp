a73a3459983ab3b13e588e84228617a31b9c168b
eugene <payeepal@hotmail.com>
2023-06-02 16:05:39 +0800

    Changed gulpfile

--- gulpfile.js
+++ gulpfile.js
@@ -1,12 +1,43 @@
-var gulp = require('gulp');
-var browserSync = require('browser-sync').create();
+const gulp = require('gulp');
+const sass = require('gulp-sass')(require('sass'));
+const autoprefixer = require('gulp-autoprefixer');
+const cleanCSS = require('gulp-clean-css');
+const uglify = require('gulp-uglify');
 
-gulp.task('browser-sync', function() {
-    browserSync.init({
-        server: {
-            baseDir: "./"
-        },
-        open: false,
-        port: 3000
-    });
-});
+// Paths
+const paths = {
+  src: {
+    sass: 'src/**/*.scss',
+    js: 'src/**/*.js'
+  },
+  dist: {
+    root: 'dist',
+    css: 'dist/css',
+    js: 'dist/js'
+  }
+};
+
+// HTML task
+function html() {
+  return gulp.src('*.html') // Update the path to match your HTML file
+    .pipe(gulp.dest(paths.dist.root));
+}
+
+// CSS task
+function css() {
+  return gulp.src(paths.src.sass)
+    .pipe(sass().on('error', sass.logError))
+    .pipe(autoprefixer())
+    .pipe(cleanCSS())
+    .pipe(gulp.dest(paths.dist.css));
+}
+
+// JS task
+function js() {
+  return gulp.src(paths.src.js)
+    .pipe(uglify())
+    .pipe(gulp.dest(paths.dist.js));
+}
+
+// Default task
+gulp.task('default', gulp.series(gulp.parallel(html, css, js)));
